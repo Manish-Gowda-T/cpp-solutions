6ad1a81f3fd75b80bfb4db8263b7c8c05e9d3b29
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-06-29 18:52:05 +0530

    GCD Checker 2 methods

--- gcd.cpp
+++ gcd.cpp
@@ -4,10 +4,19 @@ using namespace std;
 void gcd(int num1,int num2){
     for(int i=min(num1,num2); i > 0;i-- ){
         if(num1 % i ==0 && num2 % i ==0){
-                 cout<<i;
+                 cout<<i<<endl;
                  break;
         }
     }
+}
+
+void euclidian_algorithm(int a,int b){
+    while(b !=0){
+        int temp =b;
+        b = a % b;
+        a=temp;
+    }
+    cout<<a<<endl;
 }
 
 int main(){
@@ -17,4 +26,5 @@ int main(){
     cout<<"Enter the second number: ";
     cin>>num2;
     gcd(num1,num2);
+    euclidian_algorithm(num1,num2);
 }
