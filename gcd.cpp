52de1fda46df322ffcb199decf65f712576cd832
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-06-29 18:07:52 +0530

    GCD/HCF Checker

---
+++ gcd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void gcd(int num1,int num2){
+    for(int i=min(num1,num2); i > 0;i-- ){
+        if(num1 % i ==0 && num2 % i ==0){
+                 cout<<i;
+                 break;
+        }
+    }
+}
+
+int main(){
+    int num1,num2;
+    cout<<"Enter the first number: ";
+    cin>>num1;
+    cout<<"Enter the second number: ";
+    cin>>num2;
+    gcd(num1,num2);
+}
