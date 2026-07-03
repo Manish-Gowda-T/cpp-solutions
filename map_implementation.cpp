1be37978a6481c07c0c1d9ac17a3365f9798a9c2
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-07-03 13:06:31 +0530

    Basic map implementation

---
+++ map_implementation.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+   int n;
+   cin>>n;
+   int arr[n];
+
+   // load the values into the array 
+
+   for(int i =0; i <n ;i++){
+      cin>>arr[i];
+   }
+
+   //precompute
+   map<int,int> mpp;
+   for(int i=0;i<n;i++){
+      mpp[arr[i]]++;
+   }
+
+   int q;
+   cin>>q;
+
+   while(q--){
+      int number;
+      cin>>number;
+    //fetch
+
+    cout<<"Number has appeared :"<<mpp[number]<<" times";
+
+   }
+}
