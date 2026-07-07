6d4273209283f8308a3a4edadb3a2e9f71ed55e3
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-07-07 20:21:49 +0530

    Largest and 2nd largest O(n)

--- largest_in_array.cpp
+++ largest_in_array.cpp
@@ -1,26 +1,61 @@
-#include <climits>
-#include <iostream>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-  int largest = INT_MIN;
-  int index = -1;
-  int size_array;
-  std::cout << "Enter the size of the array: ";
-  std::cin >> size_array;
-  int *marks = new int[size_array];
-  std::cout << "Enter the elements of array: ";
-  for (int i = 0; i < size_array; i++) {
-    std::cin >> marks[i];
-  }
+void array_maker(vector<int>& array,int n){
+   int val;
+   cout<<"enter the elements of the array : ";
+   while(n--){
+      cin>>val;
+      array.push_back(val);
+   }
+}
 
-  for (int i = 0; i < size_array; i++) {
-    if (largest < marks[i]) {
-      largest = marks[i];
-      index = i;
-    }
-  }
-  std::cout << "largest :" << largest << std::endl;
-  std::cout << "Its Index :" << index;
+void largest_val(vector <int>& array, int n){
+   int largest=INT_MIN;
+   int index = -1;
+   for(int i=0 ; i <n ; i++){
+      if(array[i]>largest){
+         largest=array[i];
+         index=i;
+      }
+   }
+   cout<<"The largest val is : "<<largest<<endl;
+   cout<<"Its index is : "<<index<<endl;
+}
 
-  return 0;
+
+void largest_2nd_val(vector <int>& array,int n ){
+   if(n <2){
+      cout<<"No second largest ";
+      return;
+   }
+   int largest=array[0];
+   int second_largest=INT_MIN;
+   for(int i =1; i <n ; i++){
+      if(array[i] > largest ){
+         second_largest=largest;
+         largest=array[i];
+      }
+      else if( array[i] < largest && array[i] > second_largest){
+         second_largest=array[i];
+      }
+   }
+   if(second_largest==INT_MIN){
+      cout<<"No second largest element :)";
+      return;
+   }
+   cout<<"The second largest val is "<<second_largest;
+}
+
+
+int main(){
+   //take input array
+   int n;
+   vector<int> array;
+   cout<<"Enter the elements of the array : ";
+   cin>>n;
+   array_maker(array,n);
+   largest_val(array,n);
+   largest_2nd_val(array,n);
 }
